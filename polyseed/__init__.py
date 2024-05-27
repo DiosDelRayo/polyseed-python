0392685fe751a003457fb653cdcd37e433cb798b
who asks? <user@localhost>
2024-05-27 14:55:56 -0600

    break: buffer overflow

--- polyseed/__init__.py
+++ polyseed/__init__.py
@@ -11,3 +11,9 @@ from .storage import PolyseedData
 from .gf import GFPoly
 from .polyseed import Polyseed
 from .pbkdf2 import pbkdf2_sha256
+
+def generate(password: Optional[str]) -> Polyseed:
+    raise Exception('Not implemented yet, kick developers ass')  # TODO: implementation missing
+
+def recover(phrase: str, password: Optional[str]) -> Polyseed:
+    raise Exception('Not implemented yet, kick developers ass')  # TODO: implementation missing
